e5dc06c9be4752d38d0ccfc92eac14feb30283c6
Piotr Wawrzyniak <piotr@wawrzyniak.poznan.pl>
2024-06-30 19:09:39 +0200

    init maven java project, visual studio code

---
+++ hpiggwdc/src/main/java/edu/put/pw/hpiggwdc/App.java
@@ -0,0 +1,13 @@
+package edu.put.pw.hpiggwdc;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main(String[] args) {
+        int argscount = 0;
+        for (String _ : args) argscount++;
+        System.out.println("Hello World (" + argscount + ")!");
+    }
+}
